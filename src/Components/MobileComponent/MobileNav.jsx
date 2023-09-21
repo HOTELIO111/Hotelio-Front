ab70a36ad59b9138fc940d57a43cb4734d9c1be6
Sourabh Verma <89793336+Sourabh-WebDev@users.noreply.github.com>
2023-09-21 14:58:47 +0530

    SEO

--- src/Components/MobileComponent/MobileNav.jsx
+++ src/Components/MobileComponent/MobileNav.jsx
@@ -67,89 +67,84 @@ const MobileNav = () => {
                 >
                     <h1 className='mt-3' ><b>Welcome To Hotelio Rooms</b></h1>
                 </div>
-                <Grid container padding={1} spacing={2}>
-                    <Grid item xs={12}>
-
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-lg-12 px-0">
-                                    <div className={` ${style.search_form}`}>
-                                        <div>
-                                            <Grid container spacing={1}>
-                                                <Grid item xs={12}>
-                                                    <Autocomplete
-                                                        options={top100Films}
-                                                        id="disable-close-on-select"
-                                                        renderInput={(params) => (
-                                                            <TextField fullWidth {...params} label="Destination" variant="standard" />
-                                                        )}
-                                                    />
-                                                </Grid>
-                                                <Grid item xs={6}>
-                                                    <MobileDate />
-                                                </Grid>
-                                                <Grid item xs={6} className='d-flex'>
-                                                    <hr style={{ height: '50px', width: '1px', position: 'relative', left: '-4px', top: '-13px' }} />
-                                                    {/* <TextField onClick={handleShow} disabled fullWidth id="standard-read-only-input" label="Rooms and guests" value={'1 Guests 1 Room'} variant="standard"
+                <div className="w-100">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className='bg-white p-2 rounded'>
+                                <Grid container spacing={1}>
+                                    <Grid item xs={12}>
+                                        <Autocomplete
+                                            options={top100Films}
+                                            id="disable-close-on-select"
+                                            renderInput={(params) => (
+                                                <TextField fullWidth {...params} label="Destination" variant="standard" />
+                                            )}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <MobileDate />
+                                    </Grid>
+                                    <Grid item xs={6} className='d-flex'>
+                                        <hr style={{ height: '50px', width: '1px', position: 'relative', left: '-4px', top: '-13px' }} />
+                                        {/* <TextField onClick={handleShow} disabled fullWidth id="standard-read-only-input" label="Rooms and guests" value={'1 Guests 1 Room'} variant="standard"
                                                         InputProps={{
                                                             readOnly: true,
                                                             style: { userSelect: 'none' },
                                                         }}
                                                     /> */}
-                                                    <div onClick={handleShow} className='w-100'>
-                                                        <label style={Stylelabel} className="labelfordatepicker">Rooms and guests</label>
-                                                        <p style={{ marginTop: '14px' }}>{selectedGuest} Guests, {selectedRoom} Room</p>
-                                                        <hr style={{ marginTop: '11px', color: 'black' }} />
-                                                    </div>
-                                                </Grid>
-
-                                                <Grid item xs={12}>
-                                                    <LoadingButton fullWidth href='/searchedhotels' style={{ background: '#ee2e24', color: '#fff' }} >Search</LoadingButton>
-                                                </Grid>
-                                            </Grid>
-                                            <Offcanvas show={show} style={{ borderRadius: ' 25px 25px 0px 0px', height: '40vh' }} placement='bottom' onHide={handleClose}>
-                                                <Offcanvas.Header className='pt-3 pr-5' >
-                                                    <Offcanvas.Title onClick={handleClose} style={{ background: '#ee2e24', padding: '4px 76px', position: 'fixed', left: '30%', borderRadius: '5px' }}></Offcanvas.Title>
-                                                </Offcanvas.Header>
-                                                <Offcanvas.Body>
-                                                    <div className='text-center'>
-                                                        <div className='d-flex justify-content-evenly align-items-center'>
-                                                            <h5>Guest</h5>
-                                                            <FormControl className='w-25'>
-                                                                {/* <InputLabel>Select an option</InputLabel> */}
-                                                                <Select value={selectedGuest} onChange={handelChangeGuest}>
-                                                                    <MenuItem value="1">1</MenuItem>
-                                                                    <MenuItem value="2">2</MenuItem>
-                                                                    <MenuItem value="3">3</MenuItem>
-                                                                </Select>
-                                                            </FormControl>
-                                                        </div>
-                                                        <hr />
-                                                        <div className='d-flex justify-content-evenly align-items-center py-2'>
-                                                            <h5>Room</h5>
-                                                            <FormControl className='w-25'>
-                                                                {/* <InputLabel>Select an option</InputLabel> */}
-                                                                <Select value={selectedRoom} onChange={handelChangeRoom}>
-                                                                    <MenuItem value="1">1</MenuItem>
-                                                                    <MenuItem value="2">2</MenuItem>
-                                                                    <MenuItem value="3">3</MenuItem>
-                                                                    <MenuItem value="4">4</MenuItem>
-                                                                    <MenuItem value="5">5</MenuItem>
-                                                                    <MenuItem value="6">6</MenuItem>
-                                                                    <MenuItem value="7">7</MenuItem>
-                                                                </Select>
-                                                            </FormControl>
-                                                        </div>
-                                                        <Button color='error' onClick={handleClose} fullWidth variant='contained'>Done</Button>
-                                                    </div>
-                                                </Offcanvas.Body>
-                                            </Offcanvas>
+                                        <div onClick={handleShow} className='w-100'>
+                                            <label style={Stylelabel} className="labelfordatepicker">Rooms and guests</label>
+                                            <p style={{ marginTop: '14px' }}>{selectedGuest} Guests, {selectedRoom} Room</p>
+                                            <hr style={{ marginTop: '11px', color: 'black' }} />
                                         </div>
-                                    </div>
-                                </div>
+                                    </Grid>
+
+                                    <Grid item xs={12}>
+                                        <LoadingButton fullWidth href='/searchedhotels' style={{ background: '#ee2e24', color: '#fff' }} >Search</LoadingButton>
+                                    </Grid>
+                                </Grid>
+                                <Offcanvas show={show} style={{ borderRadius: ' 25px 25px 0px 0px', height: '40vh' }} placement='bottom' onHide={handleClose}>
+                                    <Offcanvas.Header className='pt-3 pr-5' >
+                                        <Offcanvas.Title onClick={handleClose} style={{ background: '#ee2e24', padding: '4px 76px', position: 'fixed', left: '30%', borderRadius: '5px' }}></Offcanvas.Title>
+                                    </Offcanvas.Header>
+                                    <Offcanvas.Body>
+                                        <div className='text-center'>
+                                            <div className='d-flex justify-content-evenly align-items-center'>
+                                                <h5>Guest</h5>
+                                                <FormControl className='w-25'>
+                                                    {/* <InputLabel>Select an option</InputLabel> */}
+                                                    <Select value={selectedGuest} onChange={handelChangeGuest}>
+                                                        <MenuItem value="1">1</MenuItem>
+                                                        <MenuItem value="2">2</MenuItem>
+                                                        <MenuItem value="3">3</MenuItem>
+                                                    </Select>
+                                                </FormControl>
+                                            </div>
+                                            <hr />
+                                            <div className='d-flex justify-content-evenly align-items-center py-2'>
+                                                <h5>Room</h5>
+                                                <FormControl className='w-25'>
+                                                    {/* <InputLabel>Select an option</InputLabel> */}
+                                                    <Select value={selectedRoom} onChange={handelChangeRoom}>
+                                                        <MenuItem value="1">1</MenuItem>
+                                                        <MenuItem value="2">2</MenuItem>
+                                                        <MenuItem value="3">3</MenuItem>
+                                                        <MenuItem value="4">4</MenuItem>
+                                                        <MenuItem value="5">5</MenuItem>
+                                                        <MenuItem value="6">6</MenuItem>
+                                                        <MenuItem value="7">7</MenuItem>
+                                                    </Select>
+                                                </FormControl>
+                                            </div>
+                                            <Button color='error' onClick={handleClose} fullWidth variant='contained'>Done</Button>
+                                        </div>
+                                    </Offcanvas.Body>
+                                </Offcanvas>
                             </div>
                         </div>
-                    </Grid>
+                    </div>
+                </div>
+                <Grid container padding={1} spacing={2}>
                     <Grid item xs={12} paddingBottom={6}>
                         <div className='text-center'>
 
