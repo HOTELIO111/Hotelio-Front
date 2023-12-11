3e010e6663e89c41b826efcbbbe083a74d4d4ebf
Sourabh Verma <89793336+Sourabh-WebDev@users.noreply.github.com>
2023-12-11 11:39:20 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // export const API_URL = "https://hotelio.onrender.com"
-export const API_URL = "http://localhost:8080";
+// export const API_URL = "http://localhost:8080";
 // export const API_URL = "http://localhost:3001";
-// export const API_URL = "https://server.hoteliorooms.com";
+export const API_URL = "https://server.hoteliorooms.com";
 export const GOOGLE_OAUTH_CLIENT_ID =
   "172857553370-86rob95it3fv2dlsvb1823lrifedl5ak.apps.googleusercontent.com";
